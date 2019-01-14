43237b05e4917cb67ebe8a0def3e6e76ea2f860d
Karel Maděra <karelmad@email.cz>
2019-01-14 13:20:58 +0100

    Changed db_config to hide my password, username etc. to be able to submit

--- db_config.php
+++ db_config.php
@@ -1,7 +1,7 @@
 <?php
   $db_config = array(
-    'server'   => 'localhost',
-    'login'    => 'maderak',
-    'password' => 'QancmsqD', // does not need to be submitted with the project
-    'database' => 'shopping_list',
+    'server'   => '',
+    'login'    => '',
+    'password' => '', // does not need to be submitted with the project
+    'database' => '',
    );
